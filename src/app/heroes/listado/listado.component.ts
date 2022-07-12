066ba384c5137eed1c591340ea06f20258cf9b9e
marciadev <marcialeite483@gmail.com>
2022-07-11 21:38:39 -0300

    First commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Capitán América', 'Doctor Strange', 'Thor', 'StarLord']
+  heroeBorrado: string = '';
+
+  borrarHeroe(): void {
+    this.heroeBorrado = this.heroes.shift() || "";
+  }
+}
